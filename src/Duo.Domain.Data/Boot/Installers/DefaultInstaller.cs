54e4a290c72a3c04d3701ac0ac37ba435ec58e64
Mauro Servienti <mauro@topics.it>
2016-01-09 15:26:56 +0100

    adds data, domain and EF migration support

---
+++ src/Duo.Domain.Data/Boot/Installers/DefaultInstaller.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.Composition;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace Duo.Domain.Data.Boot.Installers
+{
+	[Export( typeof( IWindsorInstaller ) )]
+	public class DefaultInstaller : IWindsorInstaller
+	{
+		public void Install( IWindsorContainer container, IConfigurationStore store )
+		{
+			container.Register
+			(
+				Types.FromAssemblyNamed("Duo.Domain")
+					.IncludeNonPublicTypes()
+					.Where( t => !t.IsInterface && !t.IsAbstract && !t.IsGenericType && t.Namespace != null && t.IsNested && t.Name.EndsWith( "Factory" ) )
+					.WithService.Select( ( type, baseTypes ) => new[] { type } )
+					.LifestyleSingleton()
+			);
+		}
+	}
+}
---
+++ src/Duo.Domain.Data/Services/DbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity.Infrastructure;
+using Radical.CQRS.Data;
+
+namespace Duo.Domain.Data.Services
+{
+	public class DbContextFactory : IDbContextFactory<DomainContext>
+	{
+		public DomainContext Create()
+		{
+			var ctx = new DuoDomainContext();
+			
+			return ctx;
+		}
+	}
+}
