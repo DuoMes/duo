4f434d7b2c63e38d96d36f2aa6849b45c77d3057
Mauro Servienti <mauro@topics.it>
2016-02-26 13:20:46 +0100

    aggiunto un esempio di client per le letture

--- src/Duo.Clients.Wpf/Presentation/MainViewModel.cs
+++ src/Duo.Clients.Wpf/Presentation/MainViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Duo.Messages.BobineMadri.Commands;
+using Radical.CQRS.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,8 +11,26 @@ namespace Duo.Clients.Wpf.Presentation
 {
     class MainViewModel : AbstractViewModel
     {
-        public MainViewModel(Services.AppSettings settings)
+        Services.AppSettings settings;
+        Services.BobineMadriViewsService bobineMadriViewsService;
+
+        public MainViewModel(Services.AppSettings settings, Services.BobineMadriViewsService bobineMadriViewsService)
         {
+            this.settings = settings;
+            this.bobineMadriViewsService = bobineMadriViewsService;
+        }
+
+        public async void Test()
+        {
+            var commandClient = new CommandClient(this.settings.JasonBaseAddress);
+            var result = await commandClient.ExecuteAsync<Guid>(Guid.NewGuid().ToString(), new CreaNuovaBobinaMadre()
+            {
+                Codice = "12345",
+                Fascia = 8200,
+                Lunghezza = 22000
+            });
+
+            var view = await this.bobineMadriViewsService.GetById(result);
 
         }
     }
---
+++ src/Duo.Clients.Wpf/Services/BobineMadriViewsService.cs
@@ -0,0 +1,51 @@
+﻿using Duo.Domain.ViewModels.BobineMadri;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duo.Clients.Wpf.Services
+{
+    class BobineMadriViewsService
+    {
+        Services.AppSettings settings;
+
+        public BobineMadriViewsService(Services.AppSettings settings)
+        {
+            this.settings = settings;
+        }
+
+        public async Task<BobinaMadreView> GetById(Guid id)
+        {
+            using(var apiClient = new HttpClient())
+            {
+                var url = $"{this.settings.ApiBaseAddress}bobinemadri/{id}";
+
+                var response = await apiClient.GetAsync(url);
+                var apiResult = await response.Content.ReadAsStringAsync();
+
+                var data = JsonConvert.DeserializeObject<BobinaMadreView>(apiResult);
+
+                return data;
+            }
+        }
+
+        public async Task<IEnumerable<BobinaMadreView>> List()
+        {
+            using(var apiClient = new HttpClient())
+            {
+                var url = $"{this.settings.ApiBaseAddress}bobinemadri";
+
+                var response = await apiClient.GetAsync(url);
+                var apiResult = await response.Content.ReadAsStringAsync();
+
+                var data = JsonConvert.DeserializeObject<IEnumerable<BobinaMadreView>>(apiResult);
+
+                return data;
+            }
+        }
+    }
+}
