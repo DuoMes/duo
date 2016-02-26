962efe2f4a21cf5145a879a6df65073263939e31
Mauro Servienti <mauro@topics.it>
2016-02-26 14:57:02 +0100

    clean up

--- src/Duo.Clients.Wpf/Presentation/MainViewModel.cs
+++ src/Duo.Clients.Wpf/Presentation/MainViewModel.cs
@@ -1,10 +1,6 @@
 ﻿using Duo.Messages.BobineMadri.Commands;
 using Radical.CQRS.Client;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Topics.Radical.Windows.Presentation;
 
 namespace Duo.Clients.Wpf.Presentation
