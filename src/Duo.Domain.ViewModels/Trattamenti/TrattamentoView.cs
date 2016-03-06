825936631aa6aad495a71315114b45a2acf38028
Nazareno Manco <nazareno.m@live.com>
2016-03-06 19:26:31 +0100

    Aggiunta logica gestione trattamenti

---
+++ src/Duo.Domain.ViewModels/Trattamenti/TrattamentoView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duo.Domain.ViewModels.Trattamenti
+{
+    public class TrattamentoView
+    {
+        public Guid Id { get; set; }
+        public string Codice { get; set; }
+        public string Descrizione { get; set; }
+    }
+}
---
+++ src/Duo.Server/Data/TrattamentiViewDataContext.cs
@@ -0,0 +1,30 @@
+﻿using Duo.Domain.ViewModels.Trattamenti;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+
+namespace Duo.Server.Data
+{
+    class TrattamentiViewDataContext : DbContext
+    {
+        readonly DbSet<TrattamentoView> _trattamentoViewSet;
+
+        public TrattamentiViewDataContext()
+        {
+            this._trattamentoViewSet = this.Set<TrattamentoView>();
+        }
+
+        public IQueryable<TrattamentoView> TrattamentiView
+        {
+            get { return this._trattamentoViewSet.AsNoTracking(); }
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            var trattamento = modelBuilder.Entity<TrattamentoView>();
+            trattamento.ToTable("dbo.Trattamenti");
+        }
+    }
+}
