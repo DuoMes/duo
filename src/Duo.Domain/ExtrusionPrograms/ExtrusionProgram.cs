197b55859528361e330f3659aaf53a74ac0415c2
Nazareno Manco <nazareno.m@live.com>
2017-06-03 15:50:36 +0200

    Added ExtrusionProgram Aggregate Issue #74

---
+++ src/Duo.Domain/ExtrusionPrograms/ExtrusionProgram.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Radical.CQRS;
+
+namespace Duo.Domain.ExtrusionPrograms
+{
+    class ExtrusionProgram : Aggregate<ExtrusionProgram.ExtrusionProgramState>
+    {
+        public class ExtrusionProgramState : AggregateState
+        {
+            public Guid ExtrusionLineId { get; set; }
+            public Guid ProductId { get; set; }
+            public int Length { get; set; }
+            public int Widht { get; set; }
+            public Guid TreatmentId { get; set; }
+            public int QuantityToBeProduced { get; set; }
+            public int JunboRollToBeProduced { get; set; }
+            public DateTime ExpectedProductionDate { get; set; }
+        }
+
+        public class Factory
+        {
+            public ExtrusionProgram CreateNew(Guid extrusionLineId, Guid productId, int length, int widht, 
+                Guid treatmentId, int quantityToBeProduced, int junboRollToBeProduced, DateTime expectedProductionDate)
+            {
+                var state = new ExtrusionProgramState()
+                {
+                    ExtrusionLineId = extrusionLineId,
+                    ProductId = productId,
+                    Length = length,
+                    Widht = widht,
+                    TreatmentId = treatmentId,
+                    QuantityToBeProduced = quantityToBeProduced,
+                    JunboRollToBeProduced = junboRollToBeProduced,
+                    ExpectedProductionDate = expectedProductionDate
+                };
+                var aggregate = new ExtrusionProgram(state);
+                aggregate.SetupCompleted();
+                return aggregate;
+            }
+        }
+
+        private ExtrusionProgram(ExtrusionProgram.ExtrusionProgramState state)
+            : base(state)
+        {
+        }
+
+        private void SetupCompleted()
+        {
+            this.RaiseEvent<IExtrusionProgramCreated>(e =>
+            {
+                e.ExtrusionLineId = this.Data.ExtrusionLineId;
+                e.ProductId = this.Data.ProductId;
+                e.Length = this.Data.Length;
+                e.Widht = this.Data.Widht;
+                e.TreatmentId = this.Data.TreatmentId;
+                e.QuantityToBeProduced = this.Data.QuantityToBeProduced;
+                e.JunboRollToBeProduced = this.Data.JunboRollToBeProduced;
+                e.ExpectedProductionDate = this.Data.ExpectedProductionDate;
+            });
+        }
+    }
+}
---
+++ src/Duo.Domain/ExtrusionPrograms/IExtrusionProgramCreated.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Radical.CQRS;
+
+namespace Duo.Domain.ExtrusionPrograms
+{
+    public interface IExtrusionProgramCreated : IDomainEvent
+    {
+        Guid ExtrusionLineId { get; set; }
+        Guid ProductId { get; set; }
+        int Length { get; set; }
+        int Widht { get; set; }
+        Guid TreatmentId { get; set; }
+        int QuantityToBeProduced { get; set; }
+        int JunboRollToBeProduced { get; set; }
+        DateTime ExpectedProductionDate { get; set; }
+    }
+}
